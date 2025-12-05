ac1a1d05b8a0e7df4a5d339145e711b43ea7d313
Harmmerton Kogi <hammertonkogey@gmail.com>
2025-12-05 20:12:23 +0300

    Created the home file for holding my animation code

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'screens/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animations',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Home(),
+    );
+  }
+}
